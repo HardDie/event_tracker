e89b89efeb3ea793a17e9e8c7e22257b6d2bc57b
HardDie <oleg1995sysoev@yandex.ru>
2023-01-27 11:16:49 +0300

    Replaced DB driver

--- internal/db/db.go
+++ internal/db/db.go
@@ -6,6 +6,8 @@ import (
 
 	_ "github.com/mattn/go-sqlite3"
 	//_ "github.com/glebarez/go-sqlite"
+
+	"github.com/HardDie/event_tracker/internal/logger"
 )
 
 type DB struct {
@@ -14,25 +16,22 @@ type DB struct {
 
 func Get(dbpath string) (*DB, error) {
 	flags := []string{
-		"_pragma=foreign_keys(1)",
-		"_pragma=busy_timeout(3000)",
-		"_pragma=journal_mode(WAL)",
-
-		//"_fk=true",
-		//"_timeout=5000",
-		//"_journal=WAL",
-
-		"_pragma=journal_size_limit(1073741824)", // 1Gb limit for -wal file
+		"_fk=true",      // "_pragma=foreign_keys(1)",
+		"_timeout=5000", // "_pragma=busy_timeout(3000)",
+		"_journal=WAL",  // "_pragma=journal_mode(WAL)",
 	}
 
 	db, err := sql.Open("sqlite3", dbpath+"?"+strings.Join(flags, "&"))
 	if err != nil {
 		return nil, err
 	}
-	//_, err = db.Exec("PRAGMA journal_size_limit=1073741824")
-	//if err != nil {
-	//	return nil, err
-	//}
+
+	// "_pragma=journal_size_limit(1073741824)", // 1Gb limit for -wal file
+	_, err = db.Exec("PRAGMA journal_size_limit=1073741824")
+	if err != nil {
+		logger.Error.Println("error pragma journal_size_limit:", err.Error())
+	}
+
 	return &DB{
 		DB: db,
 	}, nil
